c4e2c45336e7bf781d7f7afbc28e5580527f0449
Diego Victor de Jesus <diegovictorrj@gmail.com>
2019-04-11 02:16:10 -0300

    Correções no layout do menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
-    openSlideRef = $('#openSlideRef');
-    closeSlideRef = $('#closeSlideRef');
-    menu = $('#menu');
-    content = $('#content');
+    var filterBtn = $('#filterBtn');
+    var closeSlideRef = $('#closeSlideRef');
+    var menu = $('#menu');
+    var content = $('#content');
 
-    openSlideRef.click(() => {
+    filterBtn.click(() => {
         menu.css('width', '250px');
         content.css('margin-left', '250px');
-        openSlideRef.hide();
+        filterBtn.hide();
     });
 
     closeSlideRef.click(() => {
@@ -15,7 +15,7 @@ $(document).ready(() => {
         content.css('margin-left', '0');
 
         menu.on('webkitTransitionEnd', event => {
-            openSlideRef.show();
+            filterBtn.show();
             menu.off('webkitTransitionEnd');
         });
     });
@@ -33,4 +33,43 @@ $(document).ready(() => {
             zoom: 12
         })
     });
+
+
+    var cssmenu = $('#cssmenu > ul');
+    menu.find('.has-sub > ul').hide();
+
+    cssmenu.on('click', function (event) {
+        event.preventDefault();
+        var targetParent = $(event.target).parent();
+        if (targetParent.hasClass('has-sub')) {
+            targetParent.toggleClass('active');
+            targetParent.children('ul').slideToggle(250);
+        }
+    });
+
+    
+    
+    // TODO: requisição inicial para pegar categorias e criar tags ul
+    $.ajax({
+        url: 'https://7lxc3x2ta7.execute-api.us-east-1.amazonaws.com/Prod/listcategory',
+        success: function (response, aa, bb) {
+            console.log('success');
+            // Categorias da aplicação
+            res = response;
+            a = aa;
+            b = bb;
+            // response.forEach(element => $('#categories').append("<li><a href='#'>" + element.name + "</a></li>"));
+        }
+    });
+
+    // Pegar tweet pelo id de uma localidade
+    // https://7lxc3x2ta7.execute-api.us-east-1.amazonaws.com/Prod/gettweetbylocate?id=3
+
+    // Localidades da aplicação
+    // https://7lxc3x2ta7.execute-api.us-east-1.amazonaws.com/Prod/listlocate
+
+    // Pegar tweet pelo id de uma categoria
+    // https://7lxc3x2ta7.execute-api.us-east-1.amazonaws.com/Prod/gettweetbycategory?id=4
+
+
 });
